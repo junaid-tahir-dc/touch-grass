de934085fc589ba6f2a931396eaf0fb529684bb6
sameer0075-dc <sam@dreamcatchdev.com>
2025-11-14 15:41:37 +0500

    deployment debug

--- src/api/common/api-provider.tsx
+++ src/api/common/api-provider.tsx
@@ -6,6 +6,7 @@ export const queryClient = new QueryClient();
 
 export function APIProvider({ children }: { children: React.ReactNode }) {
   useReactQueryDevTools(queryClient);
+  console.log("")
   return (
     // Provide the client to your App
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
